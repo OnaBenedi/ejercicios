67ef8e0ade140f0aabf3ca1c5e8c9199c0644a27
OnaBenedi <onabenedi@gmail.com>
2024-06-11 10:37:01 +0200

    finished refs exercises

--- react/forms_list_styling/controlled-forms/src/components/state/Counter.jsx
+++ react/forms_list_styling/controlled-forms/src/components/state/Counter.jsx
@@ -4,19 +4,19 @@ import CounterDisplay from "./CounterDisplay";
 function Counter({ initialValue = 0, incrementBy = 1 }) {
   const [counter, setCounter] = useState(initialValue);
 
-  // const counterRef = useRef(0);
-  // useEffect(()=>{
-  //   if (counterRef.current + 1) {
-  //     console.log("up");
-  //   }
-  // }, [counter])
+   const counterRef = useRef({direction: ""});
+  
 
   function incrementCount() {
     setCounter((c) => c + incrementBy);
+    counterRef.direction = "up"
+    console.log(counterRef.direction);
   }
 
   function decrementCount() {
     setCounter((c) => c - incrementBy);
+    counterRef.direction = "down"
+    console.log(counterRef.direction);
   }
 
   function resetCount() {
