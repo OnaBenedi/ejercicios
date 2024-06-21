5e4134a626db8f20a327e3ff60ba9b900af47806
OnaBenedi <onabenedi@gmail.com>
2024-06-21 09:29:11 +0200

    finished swr exercises

---
+++ react/swr/src/components/GithubUsers.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import GitHubUser from "./GithubUser.jsx";
+
+function GitHubUsers() {
+  const [username, setUsername] = useState("");
+  const [userList, setUserList] = useState([]);
+
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUserList((currentList) => [...currentList, username]);
+    // setUserList([...userList, username])
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleUsernameChange}
+          value={username}
+          type="text"
+          placeholder="Enter a GitHub username"
+        />
+        <button>Submit</button>
+      </form>
+
+      <ul>
+        {userList.map((item, index) => {
+          return (
+            <li key={index}>
+              <GitHubUser username={item} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default GitHubUsers;
