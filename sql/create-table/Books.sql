05af583c51b8ef0f198fb8d2ce1f0d8ef2662a9d
OnaBenedi <onabenedi@gmail.com>
2024-06-26 17:22:30 +0200

    fixed first ex

--- sql/create-table/Books.sql
+++ sql/create-table/Books.sql
@@ -1,10 +1,11 @@
-CREATE TABLE Books (
-book_id INTEGER PRIMARY KEY,
-author TEXT,
-genre TEXT,
-published_year TEXT,
-isbn TEXT,
-price TEXT,
-rating TEXT,
-stock_count TEXT
+CREATE TABLE Books(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR(50),
+  author VARCHAR(50),
+  genre VARCHAR(20),
+  published_year year,
+  isbn VARCHAR(20),
+  price DECIMAL(4,2),
+  rating DECIMAL(1,1),
+  stock_count INTEGER
   )
