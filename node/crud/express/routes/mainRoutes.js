3baf2cbb33adde6d6edccdf7c9502e87efedf678
OnaBenedi <onabenedi@gmail.com>
2024-07-26 11:19:41 +0200

    finished most exercises

--- node/crud/express/routes/mainRoutes.js
+++ node/crud/express/routes/mainRoutes.js
@@ -1,30 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const mainController = require("../controllers/mainController.js")
 
-let planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
 
 //routes
-router.get("/", (req, res) => {
-  res.status(200).send("Hello!");
-}); //hacemos el path a la home
+router.get("/", mainController.home); //hacemos el path a la home
 
-router.get("/planets", (req, res) => {
-  res.json(planets);
-}); //path a planets
+router.get("/planets", mainController.showPlanetList ); //path a planets
 
-router.get("/planets/:id", (req, res) => {
-    planets.filter()
-    res.json(planets);
-  })
+router.get("/planets/:id", mainController.showPlanetById );
+
+router.get("/error", mainController.error)
+
+router.post("/planets", mainController.postNewPlanet);
+
+router.put("/planets/:id", mainController.editPlanet);
+
+router.delete("/planets/:id", mainController.deletePlanet);
 // app.post("/test", (req, res)=>{
 //     console.log(req);
 // })
